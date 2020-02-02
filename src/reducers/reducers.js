72ff853e73bf5bd79d055a676dd15940a11f1eea
jidegithub <olutolajide@gmail.com>
2020-02-02 21:03:33 +0100

    react redux added for state management

---
+++ src/reducers/reducers.js
@@ -0,0 +1,11 @@
+import { CHANGE_SEARCHFIELD } from '../constants'
+
+const initialState = {
+    searchField: ''
+}
+
+export const searchRobots = (state=initialState, action={}) => {
+    switch(action.type){
+        
+    }
+} 
