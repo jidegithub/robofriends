de180bcdc12dc97c3ce6c2c5968024ab3ac6acd3
jidegithub <olutolajide@gmail.com>
2020-02-02 13:44:22 +0100

    Updates

---
+++ precache-manifest.069844d56418c561464c007e46033ac7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "651b47014d64b13ae335",
+    "url": "/robofriends/static/css/main.032a301e.chunk.css"
+  },
+  {
+    "revision": "651b47014d64b13ae335",
+    "url": "/robofriends/static/js/main.651b4701.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.143eb19a.js"
+  },
+  {
+    "revision": "663c1169df59d57f6e7b",
+    "url": "/robofriends/static/css/2.a451bc66.chunk.css"
+  },
+  {
+    "revision": "663c1169df59d57f6e7b",
+    "url": "/robofriends/static/js/2.663c1169.chunk.js"
+  },
+  {
+    "revision": "9a708e71a878a89ae85949e3f2dbc21c",
+    "url": "/robofriends/static/media/Slackey-Regular.9a708e71.ttf"
+  },
+  {
+    "revision": "a08e40b768b10b6a55d4bc0870d304ec",
+    "url": "/robofriends/index.html"
+  }
+];
